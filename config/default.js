8295178152dee7c51c33e1c3948481ee923fd2d6
Brian McNamara <brianmay27@gmail.com>
2017-10-26 01:13:37 -0700

    Initial commit

---
+++ config/default.js
@@ -0,0 +1,15 @@
+module.exports = {
+  useHttps: true,
+  clientId: 'TV',
+  pollTime: 5000,
+  tv : {
+    ip: '192.168.1.147',
+    token: 'Zymyzcea0e'
+  },
+  mq : {
+    host : '',
+    port: '',
+    username : '',
+    token: ''
+  }
+}
---
+++ index.js
@@ -0,0 +1,61 @@
+let smartcast = require('vizio-smart-cast');
+let config = require('config');
+let btoa = require('btoa');
+var http
+if (config.useHttps) {
+  console.log('https enabled');
+  http = require('https');
+} else {
+  http = require('http');
+}
+
+console.log('configuration:' + JSON.stringify(config));
+
+let headers= {
+  Authorization: 'Basic ' + btoa(config.mq.username + ':' + config.mq.token),
+  'Content-Type' : 'application/json'
+};
+
+let http_config = {
+  host : config.mq.host,
+  port: config.mq.port,
+  path : '/api/queue/' + config.clientId,
+  method: 'get',
+  headers : headers
+};
+
+let tv = new smartcast(config.tv.ip);
+tv.pairing.useAuthToken(config.tv.token);
+
+function handleMessage(message) {
+  console.log('Handling message' + JSON.stringify(message));
+  switch (message.message) {
+    case 'powerOn':
+      console.log('turning on');
+      tv.control.power.on();
+      break;
+    case 'powerOff':
+      console.log('turning off');
+      tv.control.power.off();
+      break;
+    default:
+
+  }
+
+}
+
+(function makeRequest() {
+  console.log('Polling mq');
+  var req = http.request(http_config, (res) => {
+    res.on('data', (data) => {
+      console.log('Data recieved ' + data);
+      data = JSON.parse(data);
+      if (!!data && data.length > 0) {
+        data.forEach(handleMessage);
+      }
+    });
+    console.log('Setting timeout');
+    setTimeout(makeRequest, config.pollTime);
+  });
+  req.end();
+})();
